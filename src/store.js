91ac2b3fe3ec77cd283304d2f070ddbcfd2031fa
Jigas123 <jigspatel1177@gmail.com>
2022-07-23 13:02:24 +0530

    test commit third

--- src/store.js
+++ src/store.js
@@ -6,6 +6,7 @@ import mySaga from "./sagas";
 
 //Test commit
 //Test commit sec
+//Test commit third
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 // mount it on the Store
